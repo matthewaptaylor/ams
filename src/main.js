3802636cd8a1694aeadc7a7a505f0982769a3611
matthew <matthewaptaylor@gmail.com>
2021-08-11 14:32:24 +1200

    Installed firebase

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
 
 Vue.config.productionTip = false;
 
@@ -11,3 +13,14 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEP-66lWUOILlpK2QWLj196GJWezQPM7E",
+  authDomain: "ams-scouts-aotearoa.firebaseapp.com",
+  projectId: "ams-scouts-aotearoa",
+  storageBucket: "ams-scouts-aotearoa.appspot.com",
+  messagingSenderId: "1009744879723",
+  appId: "1:1009744879723:web:ffb8b54902f5d3ae54912e",
+  measurementId: "G-8K0GRKGLY6",
+};
+firebase.initializeApp(firebaseConfig);
