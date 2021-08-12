33a8d47ff9b6c076fa1dafd56996e2e24c7ac370
matthew <matthewaptaylor@gmail.com>
2021-08-13 09:44:58 +1200

    Moved navigation for activity view into components

---
+++ src/components/app/NavDesktop.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-sheet elevation="2" rounded class="d-none d-md-block" style="width: 14rem">
+    <v-navigation-drawer floating permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ title }}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ subtitle }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider />
+
+      <v-list dense nav>
+        <v-list-item-group color="primary">
+          <v-list-item
+            v-for="item in items"
+            :to="item.to"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
+</template>
+<style scoped></style>
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+    items: Array,
+  },
+};
+</script>
--- src/views/app/Activity.vue
+++ src/views/app/Activity.vue
@@ -1,59 +1,19 @@
 <template>
   <v-container>
     <Breadcrumbs :items="breadcrumbItems" />
-    <v-sheet elevation="2" rounded class="d-md-none mb-4 pa-4">
-      <h6 class="text-h6" style="line-height: 1.2">{{ activityName }}</h6>
-      <v-chip-group mandatory active-class="primary--text">
-        <v-chip v-for="item in navItems" :to="item.to" :key="item.title">
-          {{ item.title }}
-        </v-chip>
-      </v-chip-group>
-    </v-sheet>
+    <NavMobile :title="activityName" :items="navItems" />
     <div class="d-flex flex-wrap align-start" style="gap: 1rem">
-      <v-sheet
-        elevation="2"
-        rounded
-        class="d-none d-md-block"
-        style="width: 14rem"
-      >
-        <v-navigation-drawer floating permanent>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6">
-                {{ activityName }}
-              </v-list-item-title>
-              <v-list-item-subtitle>{{ activityDates }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-divider />
-
-          <v-list dense nav>
-            <v-list-item-group color="primary">
-              <v-list-item
-                v-for="item in navItems"
-                :to="item.to"
-                :key="item.title"
-                link
-              >
-                <v-list-item-icon>
-                  <v-icon>{{ item.icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
-      </v-sheet>
+      <NavDesktop
+        :title="activityName"
+        :subtitle="activityDates"
+        :items="navItems"
+      />
       <router-view></router-view>
     </div>
   </v-container>
 </template>
 <style scoped></style>
 <script>
-import Breadcrumbs from "../../components/app/Breadcrumbs.vue";
 import {
   mdiInformation,
   mdiClipboardList,
@@ -63,10 +23,15 @@ import {
   mdiBagPersonal,
   mdiFormSelect,
 } from "@mdi/js";
+import Breadcrumbs from "../../components/app/Breadcrumbs.vue";
+import NavMobile from "../../components/app/NavMobile.vue";
+import NavDesktop from "../../components/app/NavDesktop.vue";
 
 export default {
   components: {
     Breadcrumbs,
+    NavMobile,
+    NavDesktop,
   },
   data() {
     return {
