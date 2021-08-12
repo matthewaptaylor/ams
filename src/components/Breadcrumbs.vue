829125f1efaedea3a28bd8a12d9cbaddbcbf7e37
matthew <matthewaptaylor@gmail.com>
2021-08-12 12:49:05 +1200

    Added back button to breadcrumbs

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,17 +1,33 @@
 <template>
-  <v-breadcrumbs :items="computedItems" class="pt-0 pb-3">
-    <template v-slot:divider>
-      <v-icon>{{ chevronRightIcon }}</v-icon>
-    </template>
-  </v-breadcrumbs>
+  <div
+    class="d-flex flex-row align-center pt-0 pb-3"
+    style="gap: 0.5rem; min-height: 2rem"
+  >
+    <v-btn
+      icon
+      plain
+      v-if="computedItems.length > 1"
+      :to="computedItems[computedItems.length - 2].to"
+      class="my-n2 ms-n2"
+    >
+      <v-icon>{{ arrowLeftIcon }}</v-icon>
+    </v-btn>
+
+    <v-breadcrumbs :items="computedItems" class="pa-0">
+      <template v-slot:divider>
+        <v-icon>{{ chevronRightIcon }}</v-icon>
+      </template>
+    </v-breadcrumbs>
+  </div>
 </template>
 <style scoped></style>
 <script>
-import { mdiChevronRight } from "@mdi/js";
+import { mdiArrowLeft, mdiChevronRight } from "@mdi/js";
 
 export default {
   data() {
     return {
+      arrowLeftIcon: mdiArrowLeft,
       chevronRightIcon: mdiChevronRight,
     };
   },
